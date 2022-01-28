6b15e50c81648e8e37bfd62e5ac24263af237839
Jeffery Bennett <meandmybadself@gmail.com>
2022-01-28 16:45:53 -0600

    Cleaning up hooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { useLocalStorage } from './hooks/useLocalStorage'
 import cn from 'classnames'
-
-
-function useDebounce(value, delay) {
-    // State and setters for debounced value
-    const [debouncedValue, setDebouncedValue] = useState(value);
-
-    useEffect(
-        () => {
-            // Update debounced value after delay
-            const handler = setTimeout(() => {
-                setDebouncedValue(value);
-            }, delay);
-
-            // Cancel the timeout if value changes (also on delay change or unmount)
-            // This is how we prevent debounced value from updating if value is changed ...
-            // .. within the delay period. Timeout gets cleared and restarted.
-            return () => {
-                clearTimeout(handler);
-            };
-        },
-        [value, delay] // Only re-call effect if value or delay changes
-    );
-
-    return debouncedValue;
-}
+import { useDebounce } from './hooks/useDebounce'
 
 function compute(computation, ...message) {
     const delegate = () => {
